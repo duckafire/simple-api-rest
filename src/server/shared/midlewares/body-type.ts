4a4a6e97cf963ee464c811fcb272acaf9841d2aa
DuckAfire <155199080+duckafire@users.noreply.github.com>
2025-04-07 13:51:27 -0300

    Improve 'body-type' midleware

--- src/server/shared/midlewares/body-type.ts
+++ src/server/shared/midlewares/body-type.ts
@@ -2,7 +2,8 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
 export const bodyType = async (req: Request, res: Response, next: any) => {
-	if(req.body !== null && typeof req.body == "object" && !Array.isArray(req.body))
+	// apparently the Express convert null or undefined bodies to an empty object
+	if(typeof req.body == "object" && !Array.isArray(req.body))
 		return next();
 	else
 		return res.status(StatusCodes.BAD_REQUEST).json({ error: "the body must be an object" });
