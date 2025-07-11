04c2571cd98074769b2be2fb9663334c61e196d3
DuckAfire <155199080+duckafire@users.noreply.github.com>
2025-07-11 16:15:22 -0300

    Remove tests/*

--- tests/jest.setup.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const supertest  = require("supertest");
-const { server } = require("../build/server");
-
-const testServer = supertest(server);
-
-module.exports = { testServer };
